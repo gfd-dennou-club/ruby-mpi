0fe083e271db79f676b557aa30a0c51822e40487
Seiya Nishizawa <seiya@gfd-dennou.org>
2014-05-23 09:23:43 +0900

    use Gem::Specification.find_by_name instead of Gem.source_index for Ruby >= 2.0

--- ext/mpi/extconf.rb
+++ ext/mpi/extconf.rb
@@ -4,12 +4,20 @@ CONFIG['CC'] = "mpicc"
 gem_path = nil
 begin
   require "rubygems"
-  if (spec = Gem.source_index.find_name("narray")).any?
-    gem_path = spec.last.full_gem_path
+  if Gem::Specification.respond_to?(:find_by_name)
+    if spec = Gem::Specification.find_by_name("narray")
+      gem_path = spec.full_gem_path
+    end
+  else
+    if (spec = Gem.source_index.find_name("narray")).any?
+      gem_path = spec.full_gem_path
+    end
   end
 rescue LoadError
   dir_config("narray", Config::CONFIG["sitearchdir"])
 end
-find_header("narray.h", File.join(gem_path,"src"))
+unless find_header("narray.h", gem_path)
+  find_header("narray.h", File.join(gem_path,"src"))
+end
 
 create_makefile("mpi")
